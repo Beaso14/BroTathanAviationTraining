14a55c70ac59d36c55c97542d0e9d0ecf7c491bc
Jamie smithies <jamiesmithies@Jamies-MacBook-Air.local>
2020-11-03 00:48:14 +0000

    first_draft

---
+++ main_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request,'main_app/index.html')
+
+def about_us(request):
+    return render(request, 'main_app/about_us.html')
+
+def contact(request):
+    return render(request, 'main_app/contact.html')
+
+def courses(request):
+    return render(request, 'main_app/courses.html')
