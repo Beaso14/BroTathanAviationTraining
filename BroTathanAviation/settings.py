fb821f55d02b3b7c1a073f91704a127f760eecb9
Luke Beasley <49651291+Beaso14@users.noreply.github.com>
2022-08-29 18:59:41 +0100

    final commit

--- BroTathanAviation/settings.py
+++ BroTathanAviation/settings.py
@@ -143,11 +143,11 @@ MEDIA_URL = '/media/'
 # https://myaccount.google.com/lesssecureapps
 # https://accounts.google.com/DisplayUnlockCaptcha
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = ''
+EMAIL_HOST = 'smtp.office365.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'sales@metrologyengineeringservices.co.uk'
+EMAIL_HOST_PASSWORD = 'Metrology_'
 
 
 # Activate Django-Heroku.
--- main_app/views.py
+++ main_app/views.py
@@ -5,29 +5,49 @@ from BroTathanAviation import settings
 # Create your views here.
 
 def index(request):
-    return render(request,'main_app/index.html')
-
-def about_us(request):
-    return render(request, 'main_app/about_us.html')
-
-def contact(request):
     if request.method == "POST":
-        message_name = request.POST['message-name']
-        message_email = request.POST['message-email']
-        message = request.POST['message']
+        message_name = request.POST['name']
+        message_email = request.POST['mail']
+        message = request.POST['subject']
 
-        # send Email
+        # send Emails
         send_mail(
             'Email via Website from ' + message_name + ' ' + message_email, #subject
             message + ' Email sent from -' + message_email, #message
             message_email, #from email
             [settings.EMAIL_HOST_USER], #to email
         )
+        print("sent mail")
 
-        return render(request, 'main_app/contact.html',
+        return render(request, 'main_app/index.html',
             {'message_name': message_name})
     else:
-        return render(request, 'main_app/contact.html', {})
+        return render(request, 'main_app/index.html', {})
+
+    
+
+def about_us(request):
+    return render(request, 'main_app/about_us.html')
+
+def contact(request):
+    if request.method == "POST":
+        message_name = request.POST['name']
+        message_email = request.POST['mail']
+        message = request.POST['subject']
+
+        # send Emails
+        send_mail(
+            'Email via Website from ' + message_name + ' ' + message_email, #subject
+            message + ' Email sent from -' + message_email, #message
+            message_email, #from email
+            [settings.EMAIL_HOST_USER], #to email
+        )
+        print("sent mail")
+
+        return render(request, 'main_app/index.html',
+            {'message_name': message_name})
+    else:
+        return render(request, 'main_app/index.html', {})
 
 def courses(request):
     return render(request, 'main_app/courses.html')
